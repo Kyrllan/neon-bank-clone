ee127e65e2ebc3f89a8573ece3a9fb8b680c6f05
Kyrllan <kyrllan@hotmail.com>
2023-07-24 14:17:30 -0300

    fixed footer

--- components/AppBar/AppBar.vue
+++ components/AppBar/AppBar.vue
@@ -37,6 +37,8 @@
 import MenuMobile from './MenuMobile.vue';
 import MenuMobileItem from './MenuMobileItem.vue';
 
+import { listNeon } from '@/helpers/listNeon'
+
 components: {
   MenuMobile,
     MenuMobileItem
@@ -48,94 +50,7 @@ const router = useRouter()
 
 const menuDialog = ref(false)
 
-const menuList = [
-  {
-    name: 'Cartões', list: [
-      {
-        title: 'Cartão de crédito',
-        description: 'Sem anuidade e sem complicação.',
-        new: false
-      },
-      {
-        title: 'Cashback',
-        description: 'Dinheiro de volta nas suas compras.',
-        new: false
-      },
-      {
-        title: 'Viracrédito',
-        description: 'Investimento que vira limite.',
-        new: true
-      },
-      {
-        title: 'Cartão de débito',
-        description: 'Sem taxas e internacional.',
-        new: false
-      },
-      {
-        title: 'Cartão virtual',
-        description: 'Seguro para compras online.',
-        new: false
-      },
-    ]
-  },
-  {
-    name: 'Produtos', list: [
-      {
-        title: 'Empréstimos',
-        description: 'Simule e conheça as taxas.',
-        new: false
-      },
-      {
-        title: 'Empréstimo Consignado',
-        description: 'As menores taxas do mercado.',
-        new: false
-      },
-      {
-        title: 'Antecipação de FGTS',
-        description: 'Saiba como contratar a antecipação',
-        new: true
-      },
-      {
-        title: 'Conta digital',
-        description: 'Sem annuidade e sem tarifas.',
-        new: false
-      },
-      {
-        title: 'Investimentos',
-        description: 'Seguros, práticos e simples.',
-        new: false
-      },
-    ]
-  },
-  {
-    name: 'A Neon', list: [
-      {
-        title: 'Sobre nós',
-        description: 'Descrubra quem é a Neon!',
-        new: false
-      },
-      {
-        title: 'Carreira',
-        description: 'Confira nossas vagas abertas.',
-        new: false
-      },
-    ]
-  },
-  {
-    name: "Para MEIs", list: [
-      {
-        title: 'Conheça a MEI Fácil',
-        description: 'Descrubra quem é a MEI Fácil!',
-        new: false
-      },
-      {
-        title: 'Abra Mei',
-        description: 'É rápido, fácil  gratuito!',
-        new: false
-      },
-    ]
-  }
-]
+const menuList = listNeon
 
 
 </script>
--- components/Footer/Footer.vue
+++ components/Footer/Footer.vue
@@ -17,7 +17,43 @@
         </div>
       </div>
       <div class="mid-footer">
-        CONTEUDO
+        <div class="mid-footer-first">
+          <div class="mid-footer-first-list" v-for="(item, i) in menuList" :key="i">
+            <h3>{{ item.name }}</h3>
+            <ul v-for="(child, a) in item.list" :key="a">
+              <li>
+                {{ child.title }}
+              </li>
+            </ul>
+          </div>
+          <div class="mid-footer-first-list">
+            <h3 class="header-first-list">Perguntas</h3>
+          </div>
+          <div class="mid-footer-first-list">
+            <h3 class="header-first-list">Blog</h3>
+          </div>
+        </div>
+        <div class="mid-footer-second">
+          <div class="mr-4">
+            <h3 class="mid-footer-second-title">FALE COM A NEON</h3>
+            <ul class="mid-footer-second-text">
+              <li>oi@neon.com.br</li>
+              <li>ou acesse o app e fale com nossos</li>
+              <li>geeks.</li>
+            </ul>
+          </div>
+          <div>
+            <h3 class="mid-footer-second-title">OUTRAS INFORMAÇÕES</h3>
+            <ul class="mid-footer-second-text2">
+              <li>Aviso externo de privacidade</li>
+              <li>Termos de uso</li>
+              <li>Relatórios financeiros</li>
+              <li>Relatórios de Ouvidoria</li>
+              <li>Mapa do site</li>
+              <li>Ouvidoria</li>
+            </ul>
+          </div>
+        </div>
       </div>
     </div>
     <div class="bottom-footer">
@@ -34,11 +70,20 @@
         para CDB e Crédito Pessoal; (iv) ITAÚ UNIBANCO S.A. (CNPJ: 60.701.190/0001-04) no produto de pagamento de contas
         (Ficha de compensação e boletos de concessionárias conveniadas). Como correspondente bancário, a Neon segue as
         diretrizes da Resolução nº 4.935/21 do Banco Central do Brasil. Para entrar em contato sobre esses produtos, veja
-        os contatos aqui.
+        os contatos <span style="text-decoration: underline; cursor: pointer;">aqui.</span>
       </div>
     </div>
   </div>
 </template>
+
+<script setup>
+
+import { listNeon } from '@/helpers/listNeon'
+
+const menuList = listNeon
+
+
+</script>
 
 <style scoped lang="scss">
 #footer {
@@ -87,9 +132,70 @@
 .mid-footer {
   display: flex;
   justify-content: space-between;
-  align-items: center;
+  align-items: top;
   background-color: rgb(var(--v-theme-nBlack));
+  color: rgb(var(--v-theme-nWhite));
+  padding: 40px 0;
 
+  .mid-footer-first-list {
+    padding: 0 3rem 0 0;
+  }
+
+  ul {
+    list-style-type: none;
+
+    li {
+      padding: 0.3rem 0;
+
+      &:hover {
+        cursor: pointer;
+        text-decoration: underline;
+      }
+    }
+  }
+
+  .mid-footer-first {
+    display: flex;
+
+    .header-first-list:hover {
+      cursor: pointer;
+      text-decoration: underline;
+    }
+  }
+
+  .mid-footer-second {
+    display: flex;
+    justify-content: space-between;
+
+    .mid-footer-second-title {
+      font-family: 'Epilogue';
+      font-weight: 500;
+      font-size: 18px;
+      color: #5F5F5F;
+      padding: 0 0 10px;
+    }
+
+    .mid-footer-second-text {
+      font-family: 'Epilogue';
+      font-weight: 500;
+      font-size: 16px;
+      color: rgb(var(--v-theme-nWhite));
+      padding: 0 0 10px;
+    }
+
+    .mid-footer-second-text2 {
+      font-family: 'Epilogue';
+      font-weight: 500;
+      font-size: 20px;
+      color: rgb(var(--v-theme-nWhite));
+      padding: 0 0 10px;
+
+      & li:hover {
+        cursor: pointer;
+        text-decoration: underline;
+      }
+    }
+  }
 }
 
 
@@ -99,6 +205,7 @@
   align-items: top;
   justify-content: space-between;
   padding: 40px 35px;
+  border-top: 1px solid #6E7882;
 
   .bottom-title {
     font-family: 'Epilogue';
@@ -106,6 +213,7 @@
     font-size: 14px;
     color: rgb(var(--v-theme-nWhite));
     width: 50%;
+    padding: 0 20rem 0 0;
   }
 
   .bottom-text {
@@ -114,6 +222,7 @@
     font-size: .75rem;
     color: rgb(var(--v-theme-nWhite));
     width: 50%;
+    padding: 0 2rem 0 10rem;
   }
 }
 
@@ -139,11 +248,85 @@
     }
   }
 
+  .mid-footer {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: space-between;
+
+    .mid-footer-first {
+      display: flex;
+
+      .mid-footer-first-list {
+        padding: 0 1.5rem 0 0;
+      }
+    }
+
+    .mid-footer-second {
+      padding-top: 2rem;
+      display: flex;
+    }
+  }
+
+  .bottom-footer {
+    display: flex;
+    align-items: top;
+    justify-content: space-between;
+    padding: 40px 35px;
+
+    .bottom-title {
+      padding: 0 2rem 0 0;
+    }
+
+    .bottom-text {
+      padding: 0 2rem 0 0;
+    }
+  }
+
 
 }
 
-@media screen and (max-width: 800px) {}
+@media screen and (max-width: 800px) {
 
-@media screen and (max-width: 450px) {}
+  .mid-footer {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: space-between;
+
+    .mid-footer-first {
+      display: flex;
+      flex-direction: column;
+
+      .mid-footer-first-list {
+        padding: 1rem 1.5rem 0 0;
+      }
+    }
+
+    .mid-footer-second {
+      padding-top: 2rem;
+      display: flex;
+      flex-direction: column;
+      align-items: left;
+    }
+  }
+
+  .bottom-footer {
+    display: flex;
+    flex-direction: column;
+    padding: 2rem;
+
+    .bottom-title {
+      padding: 0;
+      width: 100%;
+    }
+
+    .bottom-text {
+      padding-top: 2rem;
+      width: 100%;
+    }
+  }
+
+}
 </style>  
 
